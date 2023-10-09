69793c37c42c856e6178dd8eda13f42173775794
Jonathan Gomez <gomez.jon480@gmail.com>
2023-10-08 19:41:43 -0700

    Include glad and change window background color

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
+
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <thread>
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
 
-    GLFWwindow* window;
-
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -22,6 +19,13 @@ int main()
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    gladLoadGL();
+
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
