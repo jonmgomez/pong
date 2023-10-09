753060a0b34748496affb907528d01b963523d15
Jonathan Gomez <gomez.jon480@gmail.com>
2023-10-08 22:35:54 -0700

    Nulls to nullptr and remove some extraneous comments

--- main.cpp
+++ main.cpp
@@ -10,14 +10,13 @@ int main()
     if (!glfwInit())
         return -1;
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (window == nullptr)
     {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
     gladLoadGL();
@@ -27,16 +26,10 @@ int main()
 
     glfwSwapBuffers(window);
 
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
