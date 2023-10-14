6a24e615c518847a7f30d2262a3a99905d0d2153
Jonathan Gomez <gomez.jon480@gmail.com>
2023-10-14 16:47:19 -0700

    Swap to glew instead of glad Primary reason is that a specific tutorial using glew is far, FAR supererior for explaining what is actually going on in these calls

--- main.cpp
+++ main.cpp
@@ -1,84 +1,49 @@
 #include <iostream>
 
-#include <glad/glad.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
-#include "ebo.h"
-#include "shader.h"
-#include "vao.h"
-#include "vbo.h"
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
 
     if (!glfwInit())
+    {
+        std::cout << "glfwInit() failure" << std::endl;
         return -1;
+    }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Hello World", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
     if (window == nullptr)
     {
+        std::cout << "glfwCreateWindow() failure" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
 
-    gladLoadGL();
-    glViewport(0, 0, 800, 800);
-
-    GLfloat vertices[] =
+    if (glewInit() != GLEW_OK)
     {
-        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
-        -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
-        0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
-        0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
-    };
+        std::cout << "glewInit() failure" << std::endl;
+        return -1;
+    }
 
-    GLuint indices[] =
-    {
-        0, 3, 5, // Lower left triangle
-        3, 2, 4, // Lower right triangle
-        5, 4, 1 // Upper triangle
-    };
-
-    Shader shaderProgram("default.vert", "default.frag");
-
-    VAO vao;
-    vao.Bind();
-
-    VBO vbo(vertices, sizeof(vertices));
-    EBO ebo(indices, sizeof(indices));
-
-    vao.LinkVBO(vbo, 0);
-
-    vao.Unbind();
-	vbo.Unbind();
-	ebo.Unbind();
+    std::cout << "Using OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
 
-        shaderProgram.Activate();
-        vao.Bind();
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
 
         glfwPollEvents();
     }
-
-    vao.Delete();
-    vbo.Delete();
-    ebo.Delete();
-    shaderProgram.Delete();
 
     glfwDestroyWindow(window);
     glfwTerminate();
