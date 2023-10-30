1686657db25ddc6f2680867fc183eca724058289
Jonathan Gomez <gomez.jon480@gmail.com>
2023-10-29 22:28:33 -0700

    Split GLFW setup into function

--- main.cpp
+++ main.cpp
@@ -38,6 +38,50 @@ nlohmann::json OpenJsonFile(const std::string& filePath)
     return jsonData;
 }
 
+GLFWwindow* SetupGLFW()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "glfwInit() failure" << std::endl;
+        return nullptr;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cerr << "glfwCreateWindow() failure" << std::endl;
+        glfwTerminate();
+        return nullptr;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, Input::KeyCallback);
+
+    // Uncomment this if you would like to blow up the GPU :D
+    // glfwSwapInterval(0);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "glewInit() failure" << std::endl;
+        return nullptr;
+    }
+
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+    std::cout << "Using OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+    int numTextureSlots;
+    glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &numTextureSlots);
+    std::cout << "Texture Slots Available: " << numTextureSlots << std::endl;
+
+    return window;
+}
+
 int main(int argc, char* argv[])
 {
     std::cout << "Hello World!" << std::endl;
@@ -54,44 +98,11 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    if (!glfwInit())
-    {
-        std::cout << "glfwInit() failure" << std::endl;
-        return -1;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    GLFWwindow* window = SetupGLFW();
     if (window == nullptr)
     {
-        std::cout << "glfwCreateWindow() failure" << std::endl;
-        glfwTerminate();
         return -1;
     }
-
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, Input::KeyCallback);
-
-    // Uncomment this if you would like to blow up the GPU :D
-    // glfwSwapInterval(0);
-
-    if (glewInit() != GLEW_OK)
-    {
-        std::cout << "glewInit() failure" << std::endl;
-        return -1;
-    }
-
-    GLCall(glEnable(GL_BLEND));
-    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-    std::cout << "Using OpenGL version: " << glGetString(GL_VERSION) << std::endl;
-
-    int numTextureSlots;
-    glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &numTextureSlots);
-    std::cout << "Texture Slots Available: " << numTextureSlots << std::endl;
 
     if (jsonData.find("shader") == jsonData.end())
     {
