7ad67ea67d71250d3852e10d2d66a544e790288f
Jonathan Gomez <gomez.jon480@gmail.com>
2023-12-08 14:23:10 -0700

    Fix up includes

--- engine/input.cpp
+++ engine/input.cpp
@@ -64,41 +64,19 @@ void Input::KeyCallback(GLFWwindow* /*window*/, int key, int /*scancode*/, int a
     mKeys[key] = (action == GLFW_PRESS || action == GLFW_REPEAT) ? InputState::Pressed : InputState::Released;
 }
 
-bool Input::CheckKeyDown(unsigned int button)
+InputState Input::GetKeyState(unsigned int keycode)
 {
-    return mKeys[button] == InputState::Pressed || mKeys[button] == InputState::Held;
+    return mKeys[keycode];
 }
 
-bool Input::CheckKeyUp(unsigned int button)
+bool Input::CheckKeyDown(unsigned int keycode)
 {
-    return mKeys[button] == InputState::Released || mKeys[button] == InputState::NotPressed;
+    return mKeys[keycode] == InputState::Pressed || mKeys[keycode] == InputState::Held;
 }
 
-// Mouse position converted to screen/world coordinates
-glm::vec3 Input::GetMousePosition()
+bool Input::CheckKeyUp(unsigned int keycode)
 {
-    return mMousePosition;
-}
-
-void Input::MousePositionCallback(GLFWwindow* /*window*/, double xPos, double yPos)
-{
-    std::cout << "Raw Mouse position: " << xPos << ", " << yPos << std::endl;
-    const float x = static_cast<float>(xPos);
-    const float y = static_cast<float>(yPos);
-
-    const int windowWidth = ApplicationWindow::GetScreenWidth();
-    const int windowHeight = ApplicationWindow::GetScreenHeight();
-    const float halfWindowWidth = windowWidth / 2.0f;
-    const float halfWindowHeight = windowHeight / 2.0f;
-
-    const float mouseWindowX = (x < halfWindowWidth) ?
-        (halfWindowWidth - x) * -2.0f : (x - halfWindowWidth) * 2.0f;
-    // y coords are positive going down, so we need to invert them
-    const float mouseWindowY = (y <= halfWindowHeight) ?
-        (halfWindowHeight - y) * 2.0f : (y - halfWindowHeight) * -2.0f;
-
-    mMousePosition.x = mouseWindowX / windowWidth * Renderer::GetXCoordMax();
-    mMousePosition.y = mouseWindowY / windowHeight * Renderer::GetYCoordMax();
+    return mKeys[keycode] == InputState::Released || mKeys[keycode] == InputState::NotPressed;
 }
 
 void Input::MouseButtonCallback(GLFWwindow* /*window*/, int button, int action, int /*mods*/)
@@ -119,6 +97,32 @@ bool Input::CheckMouseButtonDown(unsigned int button)
 bool Input::CheckMouseButtonUp(unsigned int button)
 {
     return mMouseButtons[button] == InputState::Released || mMouseButtons[button] == InputState::NotPressed;
+}
+
+void Input::MousePositionCallback(GLFWwindow* /*window*/, double xPos, double yPos)
+{
+    const float x = static_cast<float>(xPos);
+    const float y = static_cast<float>(yPos);
+
+    const int windowWidth = ApplicationWindow::GetScreenWidth();
+    const int windowHeight = ApplicationWindow::GetScreenHeight();
+    const float halfWindowWidth = windowWidth / 2.0f;
+    const float halfWindowHeight = windowHeight / 2.0f;
+
+    const float mouseWindowX = (x < halfWindowWidth) ?
+        (halfWindowWidth - x) * -2.0f : (x - halfWindowWidth) * 2.0f;
+    // y coords are positive going down, so we need to invert them
+    const float mouseWindowY = (y <= halfWindowHeight) ?
+        (halfWindowHeight - y) * 2.0f : (y - halfWindowHeight) * -2.0f;
+
+    mMousePosition.x = mouseWindowX / windowWidth * Renderer::GetXCoordMax();
+    mMousePosition.y = mouseWindowY / windowHeight * Renderer::GetYCoordMax();
+}
+
+// Mouse position converted to screen/world coordinates
+glm::vec3 Input::GetMousePosition()
+{
+    return mMousePosition;
 }
 
 } // namespace pong
--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
 #include "main.h"
 
 #include "engine.h"
-#include "config.h"
-#include "difficulty.h"
-#include "input.h"
-#include "pong.h"
-#include "renderer.h"
-#include "renderutils.h"
-#include "timer.h"
 #include "logger.h"
-#include "utils.h"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <nlohmann/json.hpp>
-
-#include <fstream>
-#include <string>
+#include <spdlog/spdlog.h>
 
 using namespace pong;
 
